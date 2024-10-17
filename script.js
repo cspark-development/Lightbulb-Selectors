dc3520bd3f8d2dafb69cc71f7ceb84dd93597ede
cspark <git@cspark.dev>
2024-10-17 13:00:01 +0100

    Implement stretch and improve code

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 // Write your code here
 let count = 0;
 
+const lightbulbs = document.querySelectorAll(".lightbulb");
+/*
 let lightbulbs = [];
 lightbulbs.push(document.querySelector(".lightbulb#lightbulb1"));
 lightbulbs.push(document.querySelector(".lightbulb#lightbulb2"));
 lightbulbs.push(document.querySelector(".lightbulb#lightbulb3"));
+*/
 const subtitle = document.querySelector(".subtitle"); 
 
-for (let i = 0; i < 3; i++) {
-	lightbulbs[i].addEventListener("click", function() {
+for (const lightbulb of lightbulbs) {
+	lightbulb.addEventListener("click", function() {
 		count++;
-		lightbulbs[i].classList.toggle("active");
-		subtitle.innerHTML = `You've clicked the lights ${count} times`;
+		lightbulb.classList.toggle("active");
+		if (count === 1) {
+			subtitle.innerHTML = `You've clicked the lights 1 time`;
+		} else {
+			subtitle.innerHTML = `You've clicked the lights ${count} times`;
+		};
+	});
+	lightbulb.addEventListener("mouseover", function() {
+		lightbulb.classList.add("active");
+	});
+	lightbulb.addEventListener("mouseout", function() {
+		lightbulb.classList.remove("active");
 	});
 };
