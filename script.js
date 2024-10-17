f34efd2ba9fb30bb5acabf7d53bbbcf0d00fe5ce
cspark <git@cspark.dev>
2024-10-17 12:52:21 +0100

    Implement code

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Write your code here
+let count = 0;
+
+let lightbulbs = [];
+lightbulbs.push(document.querySelector(".lightbulb#lightbulb1"));
+lightbulbs.push(document.querySelector(".lightbulb#lightbulb2"));
+lightbulbs.push(document.querySelector(".lightbulb#lightbulb3"));
+const subtitle = document.querySelector(".subtitle"); 
+
+for (let i = 0; i < 3; i++) {
+	lightbulbs[i].addEventListener("click", function() {
+		count++;
+		lightbulbs[i].classList.toggle("active");
+		subtitle.innerHTML = `You've clicked the lights ${count} times`;
+	});
+};
